a204960aaa3cafcd4b072d8d5c0d84a404352fe5
Siddharth Gulati <siddharth.gulati@synapse.com>
2019-06-07 17:21:01 +0530

    Graphene Relay and Nodes

---
+++ Graphene2/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+connection_string = 'postgresql://postgres:sid@localhost/5000/erp'
+
+engine = create_engine(connection_string)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+
+Base = declarative_base(cls=DeferredReflection)
+Base.query = db_session.query_property()
